4fcf0b88f396c5033c2d8743713eabac28001f04
Tom Waterman <tjwaterman99@gmail.com>
2021-07-07 13:51:55 +0200

    Fix name

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'dbt-admin=cli:main'
+            'dbt-admin=dbt_admin:main'
         ]
     },
     install_requires=['click']
