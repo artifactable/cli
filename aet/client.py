4f1936637250451528a6180868b5ed1827726597
Tom Waterman <tjwaterman99@gmail.com>
2021-07-07 19:31:51 +0200

    WIP rename

---
+++ aet/client.py
@@ -0,0 +1,28 @@
+import os
+import requests
+
+
+host = os.environ.get('AET_HOST',
+                      'https://vast-plains-60861.herokuapp.com/api')
+
+
+class Client(object):
+
+    def __init__(self, token, host=host):
+        self.token = token
+        self.host = host
+        self.headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {self.token}'
+        }
+
+    def send(self, method, path, data=None, params={}):
+        url = self.host + path
+        return requests.request(method, url, headers=self.headers, json=data,
+                                params=params)
+
+    def get(self, path, **kwargs):
+        return self.send('GET', path, **kwargs)
+
+    def post(self, path, **kwargs):
+        return self.send('POST', path, **kwargs)
--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='dbt_admin',    
+    name='aet',    
     version='0.0.1',
     description='Command line interface for working with dbt-admin.',  
     author='Tom Waterman',
     author_email='tjwaterman99@gmail.com',
-    url='https://github.com/dbt-admin/cli', 
+    url='https://github.com/aet-repos/cli', 
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'dbt-admin=dbt_admin:main'
+            'aetn=aet:main'
         ]
     },
     install_requires=['click']
