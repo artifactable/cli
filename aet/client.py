391fb666bb9d51a38f3a46535828bfe54f4563c4
Tom Waterman <tjwaterman99@gmail.com>
2021-07-13 08:07:10 +0200

    Fix url

--- aet/client.py
+++ aet/client.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 
-host = os.environ.get('AET_HOST', 'https://aet-api-prod.herokuapp.com')
+host = os.environ.get('AET_HOST', 'https://aet-api-prod.herokuapp.com/api')
 
 
 class Client(object):
