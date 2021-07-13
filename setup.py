272b529cf17c8ed3badaea6370a8358ee7fbbfba
Tom Waterman <tjwaterman99@gmail.com>
2021-07-13 14:57:48 +0200

    Bump version 0.1.1 (#8)

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aet',    
-    version='0.1.0',
+    version='0.1.1',
     description='Command line interface for working with aet services.',  
     author='Tom Waterman',
     author_email='tjwaterman99@gmail.com',
